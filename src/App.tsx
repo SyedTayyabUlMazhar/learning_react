6996c32ede93425029842c5e707744df2da60dc7
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2022-07-16 20:41:53 +0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        Hello World
+      </header>
+    </div>
+  );
+}
+
+export default App;
